ccde81635164484dd04168b20607322e4b0b3b51
Acquisition-Stack <214284651+Acquisition-Stack@users.noreply.github.com>
2026-04-05 18:14:44 +0100

    day 2

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Welcome to Cafe Spaces</p>"
+def coffee_spaces():
+    return render_template("/index.html")
+
+        # REFACTOR
+    # You should make the amount of rooms dynamic later on. 
+    # Setup a struct and have the rooms appear dynamically.
+
+@app.route("/space")
+def tokyo_night():
+    return render_template("/space.html")
+
